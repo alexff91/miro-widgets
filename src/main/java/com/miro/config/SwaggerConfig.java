6179155d647218a467530ff52a31a3b6343acf97
Alexander <alfedorov@textback.io>
2020-05-17 22:28:32 +0300

    fix swagger packaging

--- src/main/java/com/miro/config/SwaggerConfig.java
+++ src/main/java/com/miro/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.miro.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -11,7 +14,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+@EnableWebMvc
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public Docket productApi() {
@@ -28,5 +32,14 @@ public class SwaggerConfig {
                 .description("\"Spring Boot REST API for Widgets\"")
                 .version("1.0.0")
                 .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
