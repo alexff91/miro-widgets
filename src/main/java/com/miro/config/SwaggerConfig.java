1941a0c4145be3468b0d2e0a4d9621e636da455c
Alexander <alfedorov@textback.io>
2020-05-17 22:34:58 +0300

    fix conflict of swagger and zuul

--- src/main/java/com/miro/config/SwaggerConfig.java
+++ src/main/java/com/miro/config/SwaggerConfig.java
@@ -2,9 +2,6 @@ package com.miro.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -14,8 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@EnableWebMvc
-public class SwaggerConfig extends WebMvcConfigurerAdapter {
+public class SwaggerConfig {
 
     @Bean
     public Docket productApi() {
@@ -32,14 +28,5 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
                 .description("\"Spring Boot REST API for Widgets\"")
                 .version("1.0.0")
                 .build();
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
